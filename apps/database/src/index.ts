f213ad53ffe6ad20d5ad5d49069176a49dd67a55
alexandreprojetoseprocessos-netizen <alexandre.projetoseprocessos@gmail.com>
2026-01-28 10:24:16 -0300

    atualização 28 01 26 10:24

--- apps/database/src/index.ts
+++ apps/database/src/index.ts
@@ -6,19 +6,4 @@ const rootEnvPath = path.resolve(__dirname, "../../../.env");
 dotenv.config({ path: rootEnvPath });
 dotenv.config();
 
-const buildDatabaseUrl = (url?: string) => {
-  if (!url) return url;
-  const [base, query] = url.split("?");
-  const params = new URLSearchParams(query ?? "");
-  if (!params.has("connection_limit")) params.set("connection_limit", "5");
-  if (!params.has("pool_timeout")) params.set("pool_timeout", "20");
-  return `${base}?${params.toString()}`;
-};
-
-export const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: buildDatabaseUrl(process.env.DATABASE_URL)
-    }
-  }
-});
+export const prisma = new PrismaClient();
