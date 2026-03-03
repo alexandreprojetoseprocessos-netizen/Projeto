0995190d8a2b21354b54e13df926c1c4686161ba
alexandreprojetoseprocessos-netizen <alexandre.projetoseprocessos@gmail.com>
2026-03-03 14:58:26 -0300

    Front: remove legado JS apos migracao para TS

--- apps/front/src/pages/NotFoundPage.js
+++
@@ -1,3 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-const NotFoundPage = () => (_jsxs("div", { className: "page-card", children: [_jsx("h1", { children: "P\u00E1gina n\u00E3o encontrada" }), _jsx("p", { className: "muted", children: "Verifique o endere\u00E7o ou use o menu lateral para navegar." })] }));
-export default NotFoundPage;
